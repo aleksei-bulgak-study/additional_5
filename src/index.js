9e3eae1c7bc19bf0d338e9912bfe6902f250111c
aleksei.bulgak <a.bulgak@intetics.com>
2018-09-12 22:51:15 +0300

    implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var buffer = [];
+  str.split('').forEach(element => {
+    if (buffer.length > 0 && searchForKeyValue(buffer[buffer.length - 1], bracketsConfig) == element) {
+      buffer.pop();
+      return;
+    }
+    buffer.push(element);
+  });
+  return buffer.length == 0;
+}
+
+function searchForKeyValue(key, bracketsConfig){
+  let result = bracketsConfig.find(element => element[0] == key);
+  return result ? result[1] : null;
 }
